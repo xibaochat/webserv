ea5943559cac828b28ea7311960e36aa572bcce4
ypan <yutingpan@hotmail.com>
2022-02-06 23:26:30 +0100

    test post

---
+++ file_upload.py
@@ -0,0 +1,19 @@
+import cgi
+import cgitb
+# This activates a special handler
+# that will display detailed reports
+# in the web browser if any errors occur
+cgitb.enable()
+
+data = cgi.FieldStorage()
+
+print('Content-Type:text/html\r\n\r\n')
+print('<html>\n')
+print('<head>\n')
+print('<title>Hello World - First CGI Program</title>\n')
+print('</head>\n')
+print('<body>\n')
+print('<h2>haha' + str(data)  + ' This is my first CGI program</h2>\n')
+print('</body>\n')
+print('</html>\n')
+
